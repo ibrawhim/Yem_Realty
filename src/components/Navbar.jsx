25548f35d90a54fbdaa321a843a730beaa7492cf
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-11-24 00:16:41 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,9 +59,8 @@ const Navbar = () => {
             </div>
             <a className='hidden md:block bg-red-500 px-2 my-6 h-[25px] rounded font-semibold' href="">Join Us</a>
             {/* <div className='md:hidden my-7'><FaBars /></div> */}
-        </nav>
-        <section className='absolute top-[50px] hidden' id='firstSection'>
-          <div className='border flex p-5 mx-80 gap-20 justify-center items-center bg-white rounded z-[2] relative'>
+        <section className='absolute  top-[50px] hidden' id='firstSection'>
+          <div className='border w-[400px] flex p-5 ms-40 me-20 gap-20 justify-center items-center bg-white rounded z-[2] relative'>
             <div>
               <div className='flex gap-2'>
               <span className='mt-1 text-red-500'><FaHouse /></span><span>Properties</span>
@@ -76,7 +75,7 @@ const Navbar = () => {
             </div>
           </div>
         </section>
-        <section className='absolute top-[50px] left-[580px] border p-5 bg-white rounded hidden' id='secondSection'>
+        <section className='absolute w-[200px] top-[50px] left-[530px]  border p-5 bg-white rounded hidden' id='secondSection'>
           <div className='flex  flex-col'>
           <div className='flex gap-3 mb-5 hover:bg-gray-100 p-1 rounded'>
             <p className='text-red-500 mt-1'><TbHelpHexagon /></p>
@@ -88,6 +87,7 @@ const Navbar = () => {
           </div>
           </div>
         </section>
+        </nav>
     </div>
     </>
   )
