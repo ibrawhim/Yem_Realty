c36823afdfd61b848fff576b73feaf2d12e21153
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-11-29 23:50:26 +0100

    navbar

--- src/pages/Carousel.jsx
+++ src/pages/Carousel.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import main from '../images/1/main.png'
 import one from '../images/1/one.png'
 import two from '../images/1/two.png'
@@ -9,17 +9,24 @@ import {BsChevronCompactLeft} from 'react-icons/bs'
 import {BsChevronCompactRight} from 'react-icons/bs'
 
 const Carousel = () => {
+    const [currentIndex, setCurrentIndex] = useState(0)
 
     let slides = [
         one,two,four,five
     ]
 
+    const prevSlide = () => {
+        const firstSlide = currentIndex===0;
+        const newIndex = firstSlide ? slides.length - 1 : currentIndex - 1
+        setCurrentIndex(newIndex)
+    }
+
   return (
     <>
-        <div className='flex max-w-[140px] h-[170px] w-full m-auto relative'>
-            <div style={{backgroundImage: `url(${slides[3]})`}} className='w-full h-full bg-center bg-cover duration-500'></div>
-            <div><BsChevronCompactLeft/></div>
-            <div><BsChevronCompactRight/></div>
+        <div className='flex max-w-[200px] h-[170px] w-full m-auto relative border'>
+            <div style={{backgroundImage: `url(${slides[currentIndex]})`}} className='w-full h-full bg-center bg-cover duration-500'></div>
+            <div onClick={prevSlide} className='absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5  p-1 cursor-pointer rounded-full bg-black text-white text-2xl'><BsChevronCompactLeft/></div>
+            <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 p-1 cursor-pointer  rounded-full bg-black text-white text-2xl'><BsChevronCompactRight/></div>
         </div>
     </>
   )
