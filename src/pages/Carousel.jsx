870b5d037df3730ef646517ea21f4967e29ea2f5
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-11-29 23:21:55 +0100

    navbar

--- src/pages/Carousel.jsx
+++ src/pages/Carousel.jsx
@@ -5,6 +5,8 @@ import two from '../images/1/two.png'
 import three from '../images/1/three.png'
 import four from '../images/1/four.png'
 import five from '../images/1/five.png'
+import {BsChevronCompactLeft} from 'react-icons/bs'
+import {BsChevronCompactRight} from 'react-icons/bs'
 
 const Carousel = () => {
 
@@ -14,14 +16,10 @@ const Carousel = () => {
 
   return (
     <>
-        <div className='flex overflow-hidden'>
-            {
-                slides.map((item)=>(
-                     <Carousel className='w-full'>
-                        <img src={item} className='w-[100%]' alt="" />
-                    </Carousel>
-                ))
-            }
+        <div className='flex max-w-[140px] h-[170px] w-full m-auto relative'>
+            <div style={{backgroundImage: `url(${slides[3]})`}} className='w-full h-full bg-center bg-cover duration-500'></div>
+            <div><BsChevronCompactLeft/></div>
+            <div><BsChevronCompactRight/></div>
         </div>
     </>
   )
