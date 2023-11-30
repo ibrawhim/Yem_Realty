285636503e7a941380db1f14699b174051731f04
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-11-30 14:54:04 +0100

    icon

--- src/pages/CarouselList.jsx
+++ src/pages/CarouselList.jsx
@@ -12,6 +12,20 @@ import afour from '../images/4/d.jpg'
 import afive from '../images/4/e.jpg'
 import asix from '../images/4/f.jpg'
 import aseven from '../images/4/g.jpg'
+import oone from '../images/5/oone.jpg'
+import otwo from '../images/5/otwo.jpg'
+import othree from '../images/5/othree.jpg'
+import ofour from '../images/5/ofour.jpg'
+import ofive from '../images/5/ofive.jpg'
+import osix from '../images/5/osix.jpg'
+import oseven from '../images/5/oseven.jpg'
+import eone from '../images/6/eone.jpg'
+import etwo from '../images/6/etwo.jpg'
+import ethree from '../images/6/ethree.jpg'
+import efour from '../images/6/efour.jpg'
+import efive from '../images/6/efive.jpg'
+import esix from '../images/6/esix.jpg'
+import eseven from '../images/6/eseven.jpg'
 
 export const carouselList = [
     {
@@ -25,5 +39,17 @@ export const carouselList = [
         price: '100,000,000',
         area: 'Jericho Estate Ibadan',
         images: [aone,atwo,athree,afour,afive,asix,aseven]
+    },
+    {
+        type: '4 Bedroom House',
+        price: '90,000,000',
+        area: 'Jericho GRA Ibadan',
+        images: [oone,otwo,othree,ofour,ofive,osix,oseven]
+    },
+    {
+        type: '5 Bedroom House',
+        price: '75,000,000',
+        area: 'Akobo Ibadan',
+        images: [eone,etwo,ethree,efour,efive,esix,eseven]
     },
 ]
