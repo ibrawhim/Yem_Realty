36aa365bfd01348013ef8c6ade7e24ea1de74273
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-12-08 00:34:03 +0100

    redux

--- src/redux/admin.js
+++ src/redux/admin.js
@@ -1,1 +1,23 @@
 import {createSlice} from '@reduxjs/toolkit'
+
+const initialData =()=>   {
+
+    const item = window.localStorage.getItem('adminSignUp')
+    
+    return item ? JSON.parse(item) : {
+            currentStep: 0,
+            
+            fullName: '',
+            Email: '',
+            Phone: '',
+            
+            whatAreYouSelling: '',
+            LGA: '',
+            planAmount: 0,
+            
+            addOnTypes: [],
+            addOnTotalAmount: 0
+        }
+    
+    }
+    
