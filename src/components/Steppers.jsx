a49fbf7d24d64cb78d54da4795b1e718e780ddcb
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-12-10 01:34:09 +0100

    redux steppers

--- src/components/Steppers.jsx
+++ src/components/Steppers.jsx
@@ -16,12 +16,12 @@ const Steppers = ({currentIndex}) => {
   
   return (
     <>
-       <div className='w-full border'>
-            <section className='flex flex-row justify-center'>
+       <div className='w-full'>
+            <section className='flex flex-row justify-center '>
                 <div className=' px-4 shadow'>
                    {
                     steps.map((items,i)=>(
-                      <div key={i} className='flex py-5 lg:w-40'>
+                      <div key={i} className='flex py-5 lg:w-40 mt-4'>
                                 <div className={`${currentIndex === i && 'bg-black text-white' } w-[35px] text-center font-bold flex sm justify-center items-center mt-4 border h-[35px] rounded-full lg:me-2 me-0`}>{i + 1}</div>
                                 <div className='hidden lg:block text-center'>
                                 <div className='step1 font-bold text-white'>STEP{i + 1}</div>
