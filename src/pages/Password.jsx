e3aa33e02dfa09cf17864aecca5864820c002183
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-12-14 13:06:59 +0100

    redux steppers

--- src/pages/Password.jsx
+++ src/pages/Password.jsx
@@ -14,19 +14,20 @@ const Password = () => {
   // console.log(store);
   useEffect(() => {
     if (store) {
-        setValue("dob", store.dob)
-        setValue("gender", store.gender)
+        setValue("password", store.password)
+        // setValue("gender", store.gender)
     }
   }, [])
 
 
-//   const Schema = yup.object().shape({
-//     dob: yup.string().required( <span className='flex'><span className='mt-1 me-1'><MdErrorOutline /></span> <span>Select Date of Birth</span></span> ),
-//     gender: yup.string().required( <span className='flex'><span className='mt-1 me-1'><MdErrorOutline /></span> <span>Select a Gender</span></span> )
-//   })
-//   const { register, handleSubmit, setValue, formState: { errors }, } = useForm({
-//     resolver: yupResolver(Schema)
-//   })
+  const Schema = yup.object().shape({
+    password: yup.string().required( <span className='flex'><span className='mt-1 me-1'><MdErrorOutline /></span> <span>Please Enter a Password</span></span>),
+    confirm: yup.string().required( <span className='flex'><span className='mt-1 me-1'><MdErrorOutline /></span> <span>confirm your Password</span></span>),
+    
+  })
+  const { register, handleSubmit, setValue, formState: { errors }, } = useForm({
+    resolver: yupResolver(Schema)
+  })
   
 
 
@@ -40,17 +41,17 @@ const onSubmit = (myData) => {
   return (
     <>
         <div className='lg:w-[300px] md:w-[300px] w-[250px] h-[340px]'>
-          <h1 className='text-center text-xl font-bold text-red-700'>Your Info (Contd.)</h1>
+          {/* <h1 className='text-center text-xl font-bold text-red-700'>Your Info (Contd.)</h1> */}
               <form action="" onSubmit={handleSubmit(onSubmit)}>
                     <div className='my-2'>
                         <label className='font-bold' htmlFor="">Password</label><br />
-                        <input {...register("password")} type="date" className={`border border-black h-[40px] mt-3 rounded w-full`}/>
+                        <input {...register("password")} type="text" className={`border border-black h-[40px] mt-3 rounded w-full`}/>
                         <p className='text-red-600'>{errors.password?.message}</p>
                     </div>
                     <div className='my-2'>
                         <label className='font-bold' htmlFor="">Confirm</label><br />
-                        <input type="date" className={`border border-black h-[40px] mt-3 rounded w-full`}/>
-                        <p className='text-red-600'>{errors.password?.message}</p>
+                        <input  {...register("password")} type="text" className={`border border-black h-[40px] mt-3 rounded w-full`}/>
+                        <p className='text-red-600'>{errors.confirm?.message}</p>
                     </div> 
                     <button onClick={()=>dispatch(handlePreviousStep())} className=' bg-red-700 text-white rounded p-2 mt-[35%]'>Previous</button>
                     <button type='submit' className='float-right bg-red-700 text-white rounded p-2 mt-[35%]'>Next</button>
