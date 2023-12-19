1bade63acf6356dd11993d9a8b666713e353ae5d
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-12-19 13:54:58 +0100

    clients

--- src/pages/TermsAndConditionx.jsx
+++ src/pages/TermsAndConditionx.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
-
+// import '../App.css'
 const TermsAndConditionx = () => {
   return (
     <>
-        <div>
-            <h1 className='text-center font-bold text-2xl'>Terms and Conditions</h1>
+      <div>
+        <div className='bg-red-700'>
+            <h1 className='flex  justify-center text-white items-center font-bold text-3xl w-full lg:h-[300px] md:h-[180px] h-[100px]'>Terms and Conditions</h1>
         </div>
+        <div className='myDiv my-10 mx-10'>
+            <div>
+              <h1> 1. Acceptance of Terms </h1>
+              <p>By accessing and using the Yem Realty website ("the Website"), you agree to comply with and be bound by the following terms and conditions. If you do not agree to these terms, please refrain from using the Website.</p>
+            </div>
+            <div>
+              <h1> 2. Use of the Website </h1>
+              <p>2.1. Yem Realty grants you a non-exclusive, revocable license to access and use the Website for personal and non-commercial purposes.</p>
+              <p> 2.2. You agree not to use the Website for any unlawful or prohibited purpose or in any manner that could damage, disable, overburden, or impair the functionality of the Website.</p>
+            </div>
+            <div>
+              <h1> 3. User Accounts </h1>
+              <p> 3.1. To access certain features of the Website, you may be required to register for an account. You agree to provide accurate, current, and complete information during the registration process.</p>
+              <p> 3.2. You are responsible for maintaining the confidentiality of your account and password and for restricting access to your computer or device. You agree to accept responsibility for all activities that occur under your account or password.</p>
+            </div>
+            <div>
+              <h1> 4. Property Listings </h1>
+              <p> 4.1. Yem Realty may display property listings provided by third-party agents, brokers, or users. Yem Realty does not guarantee the accuracy or completeness of such listings and is not responsible for any errors or omissions.</p>
+              <p> 4.2. Yem Realty reserves the right to remove or modify any property listing at its discretion.</p>
+            </div>
+            <div>
+              <h1> 5. Intellectual Property </h1>
+              <p> 5.1. The content on the Website, including text, graphics, logos, and images, is the property of Yem Realty and is protected by intellectual property laws.</p>
+              <p> 5.2. You may not reproduce, distribute, display, or create derivative works of any part of the Website without the prior written consent of Yem Realty.</p>
+            </div>
+            <div>
+              <h1> 6. Privacy Policy </h1>
+              <p>Please review our Privacy Policy, which governs the collection, use, and disclosure of personal information on the Website.</p>
+            </div>
+            <div>
+              <h1> 7. Limitation of Liability </h1>
+              <p> 7.1. Yem Realty is not liable for any direct, indirect, incidental, consequential, or punitive damages arising out of your use or inability to use the Website.</p>
+              <p> 7.2. Yem Realty is not responsible for the actions, content, information, or data of third parties and you release us, our directors, officers, employees, and agents from any claims and damages, known and unknown, arising out of or in any way connected with any claim you have against such third parties.</p>
+            </div>
+            <div>
+              <h1> 8. Changes to Terms and Conditions </h1>
+              <p> Yem Realty reserves the right to modify or replace these terms and conditions at any time. Your continued use of the Website after any changes constitute your acceptance of the new terms.</p>
+            </div>
+            <div>
+              <h1> 9. Governing Law </h1>
+              <p> These terms and conditions are governed by and construed in accordance with the laws of [jurisdiction], and you submit to the exclusive jurisdiction of the courts located within [jurisdiction] for the resolution of any disputes.</p>
+            </div>
+
+            <p>Last Updated: December 20, 2023</p>
+
+          
+
+
+        </div>
+      </div>
     </>
   )
 }
